eb02afd7e683a410c6b11dded481f3c417908469
Jose Navas <josenavasmolina@gmail.com>
2016-12-19 09:42:52 -0800

    Commenting all the code on that file to see something?? Completely lost...

--- qtp_biom/tests/test_plugin.py
+++ qtp_biom/tests/test_plugin.py
@@ -35,95 +35,87 @@ class PluginTests(PluginTestCase):
                 else:
                     remove(fp)
 
-    def _wait_job(self, job_id):
-        for i in range(5):
-            status = self.qclient.get_job_info(job_id)['status']
-            if status != 'running':
-                break
-            sleep(0.5)
-            print "TRYING TO DEBUG IN HERE"
-        return status
-
-    def test_execute_job_summary(self):
-        # Create a summary job
-        data = {'command': dumps(['BIOM type', '2.1.4',
-                                  'Generate HTML summary']),
-                'parameters': dumps({'input_data': 4}),
-                'status': 'queued'}
-        job_id = self.qclient.post(
-            '/apitest/processing_job/', data=data)['job']
-
-        plugin("https://localhost:21174", job_id, self.out_dir)
-
-        obs = self._wait_job(job_id)
-        self.assertEqual(obs, 'success')
-
-    def test_execute_job_validate(self):
-        # Create a prep template
-        prep_info = {'SKB8.640193': {'col': 'val1'},
-                     'SKD8.640184': {'col': 'val2'}}
-        data = {'prep_info': dumps(prep_info),
-                'study': 1,
-                'data_type': '16S'}
-        template = self.qclient.post(
-            '/apitest/prep_template/', data=data)['prep']
-        # Create a new validate job
-        fd, biom_fp = mkstemp(suffix=".biom")
-        close(fd)
-        data = np.random.randint(100, size=(2, 2))
-        table = Table(data, ['O1', 'O2'], ['SKB8.640193', 'SKD8.640184'])
-        with biom_open(biom_fp, 'w') as f:
-            table.to_hdf5(f, "Test")
-        data = {'command': dumps(['BIOM type', '2.1.4', 'Validate']),
-                'parameters': dumps(
-                    {'files': dumps({'biom': [biom_fp]}),
-                     'template': template,
-                     'artifact_type': 'BIOM'}),
-                'artifact_type': 'BIOM',
-                'status': 'queued'}
-        job_id = self.qclient.post(
-            '/apitest/processing_job/', data=data)['job']
-
-        plugin("https://localhost:21174", job_id, self.out_dir)
-        obs = self._wait_job(job_id)
-        self.assertEqual(obs, 'success')
-
-    def test_execute_job_error(self):
-        # Create a prep template
-        print "START TEST"
-        prep_info = {'SKB8.640193': {'col': 'val1'},
-                     'SKD8.640184': {'col': 'val2'}}
-        data = {'prep_info': dumps(prep_info),
-                'study': 1,
-                'data_type': '16S'}
-        template = self.qclient.post(
-            '/apitest/prep_template/', data=data)['prep']
-        print "PREP TEMPLATE CREATED"
-        # Create a new validate job
-        fd, biom_fp = mkstemp(suffix=".biom")
-        close(fd)
-        data = np.random.randint(100, size=(2, 2))
-        table = Table(data, ['O1', 'O2'], ['S1', 'S2'])
-        with biom_open(biom_fp, 'w') as f:
-            table.to_hdf5(f, "Test")
-        print "BIOM TABLE CREATED"
-        data = {'command': dumps(['BIOM type', '2.1.4', 'Validate']),
-                'parameters': dumps(
-                    {'files': dumps({'biom': [biom_fp]}),
-                     'template': template,
-                     'artifact_type': 'BIOM'}),
-                'artifact_type': 'BIOM',
-                'status': 'queued'}
-        job_id = self.qclient.post(
-            '/apitest/processing_job/', data=data)['job']
-        print "JOB CREATED"
-
-        print "WHAT ABOUT HERE?"
-        plugin("https://localhost:21174", job_id, self.out_dir)
-        print "HEY DONE!!!"
-        obs = self._wait_job(job_id)
-        print "I CAN'T WAIT ANYMORE"
-        self.assertEqual(obs, 'error')
+    # def _wait_job(self, job_id):
+    #     for i in range(5):
+    #         status = self.qclient.get_job_info(job_id)['status']
+    #         if status != 'running':
+    #             break
+    #         sleep(0.5)
+    #     return status
+    #
+    # def test_execute_job_summary(self):
+    #     # Create a summary job
+    #     data = {'command': dumps(['BIOM type', '2.1.4',
+    #                               'Generate HTML summary']),
+    #             'parameters': dumps({'input_data': 4}),
+    #             'status': 'queued'}
+    #     job_id = self.qclient.post(
+    #         '/apitest/processing_job/', data=data)['job']
+    #
+    #     plugin("https://localhost:21174", job_id, self.out_dir)
+    #
+    #     obs = self._wait_job(job_id)
+    #     self.assertEqual(obs, 'success')
+    #
+    # def test_execute_job_validate(self):
+    #     # Create a prep template
+    #     prep_info = {'SKB8.640193': {'col': 'val1'},
+    #                  'SKD8.640184': {'col': 'val2'}}
+    #     data = {'prep_info': dumps(prep_info),
+    #             'study': 1,
+    #             'data_type': '16S'}
+    #     template = self.qclient.post(
+    #         '/apitest/prep_template/', data=data)['prep']
+    #     # Create a new validate job
+    #     fd, biom_fp = mkstemp(suffix=".biom")
+    #     close(fd)
+    #     data = np.random.randint(100, size=(2, 2))
+    #     table = Table(data, ['O1', 'O2'], ['SKB8.640193', 'SKD8.640184'])
+    #     with biom_open(biom_fp, 'w') as f:
+    #         table.to_hdf5(f, "Test")
+    #     data = {'command': dumps(['BIOM type', '2.1.4', 'Validate']),
+    #             'parameters': dumps(
+    #                 {'files': dumps({'biom': [biom_fp]}),
+    #                  'template': template,
+    #                  'artifact_type': 'BIOM'}),
+    #             'artifact_type': 'BIOM',
+    #             'status': 'queued'}
+    #     job_id = self.qclient.post(
+    #         '/apitest/processing_job/', data=data)['job']
+    #
+    #     plugin("https://localhost:21174", job_id, self.out_dir)
+    #     obs = self._wait_job(job_id)
+    #     self.assertEqual(obs, 'success')
+    #
+    # def test_execute_job_error(self):
+    #     # Create a prep template
+    #     prep_info = {'SKB8.640193': {'col': 'val1'},
+    #                  'SKD8.640184': {'col': 'val2'}}
+    #     data = {'prep_info': dumps(prep_info),
+    #             'study': 1,
+    #             'data_type': '16S'}
+    #     template = self.qclient.post(
+    #         '/apitest/prep_template/', data=data)['prep']
+    #     # Create a new validate job
+    #     fd, biom_fp = mkstemp(suffix=".biom")
+    #     close(fd)
+    #     data = np.random.randint(100, size=(2, 2))
+    #     table = Table(data, ['O1', 'O2'], ['S1', 'S2'])
+    #     with biom_open(biom_fp, 'w') as f:
+    #         table.to_hdf5(f, "Test")
+    #     data = {'command': dumps(['BIOM type', '2.1.4', 'Validate']),
+    #             'parameters': dumps(
+    #                 {'files': dumps({'biom': [biom_fp]}),
+    #                  'template': template,
+    #                  'artifact_type': 'BIOM'}),
+    #             'artifact_type': 'BIOM',
+    #             'status': 'queued'}
+    #     job_id = self.qclient.post(
+    #         '/apitest/processing_job/', data=data)['job']
+    #
+    #     plugin("https://localhost:21174", job_id, self.out_dir)
+    #     obs = self._wait_job(job_id)
+    #     self.assertEqual(obs, 'error')
 
 if __name__ == '__main__':
     main()

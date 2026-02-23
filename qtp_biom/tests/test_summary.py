5970024c9e883963f58b668fb210d921d7829503
Stefan Janssen <stefan.m.janssen@gmail.com>
2026-02-23 16:50:17 +0100

    debug codebase

--- qtp_biom/tests/test_summary.py
+++ qtp_biom/tests/test_summary.py
@@ -78,7 +78,9 @@ class SummaryTestsWith(PluginTestCase):
 
         # load phylogeny
         tree = TreeNode.read(fp_tree)
-
+        import sys
+        import os
+        print("STEFAN: fp_biom=%s, pwd=%s" % (fp_biom, os.getcwd()), file=sys.stderr)
         obs_index_fp, obs_viz_fp, qza_fp = _generate_html_summary(
             fp_biom, md, self.out_dir, True, tree=tree)
 
